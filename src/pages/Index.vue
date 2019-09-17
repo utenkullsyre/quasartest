42b80d846e8e7e68862e337816f1e0eb40af51a1
Børset Tor Røkkum <TOBORS@vegvesen.no>
2019-09-17 22:45:54 +0200

    First commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="flex flex-center column ">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" class="q-pa-lg">
+    <q-card dark bordered class="bg-red-9 q-pa-md shadow-24">
+      <q-card-section>
+        <q-spinner
+          color="accent"
+          size="3rem"
+        />
+
+        <q-spinner
+          color="primary"
+          size="3em"
+          :thickness="2"
+        />
+
+        <q-spinner
+          color="primary"
+          size="3em"
+          :thickness="10"
+        />
+      </q-card-section>
+    </q-card>
+
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
